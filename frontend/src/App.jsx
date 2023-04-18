74b994ed7fd283ec7cdd5dd1c2f0498151bda337
PGS <sarangipriyanshu@gmail.com>
2023-04-18 18:24:43 +0530

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
- 
+
 import './index.css'
- 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Navbar from './Component/Navbar/Navbar'
 import Landing from './Component/Middle/Landing'
 import Footer from './Component/Footer/Footer'
-
+import  MultistepContext  from './StepContext/UserContext'
 const App = () => {
   return (
 
 
     <div>
-      
-        <Navbar />
-        <Landing />
-      <Footer/>
-       
+       <MultistepContext>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' component={Landing} />
 
+
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+     </MultistepContext>
     </div>
 
 
