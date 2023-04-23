e00a2d9369498d9b2f5a90fdc49bcaed437edfa5
PGS <sarangipriyanshu@gmail.com>
2023-04-23 16:15:37 +0530

    context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './Component/Navbar/Navbar'
 import Landing from './Component/Middle/Landing'
 import Footer from './Component/Footer/Footer'
+import  MultistepContext  from './StepContext/UserContext'
  
 const App = () => {
   return (
 
 
     <div>
-      
+      <MultistepContext>
         <BrowserRouter>
           <Navbar />
           <Routes>
             <Route path='/'  element={<Landing/>} />
-
-
           </Routes>
           <Footer />
         </BrowserRouter>
-   
+   </MultistepContext>
     </div>
 
 
